9de84b1390a0ad55bd9ec0def498e429faf7b33f
Shay Narang <shaynarang@admins-MacBook-Pro-3.local>
2017-03-23 20:40:21 -0500

    customize admin appointments

--- app/admin/appointment.rb
+++ app/admin/appointment.rb
@@ -1,15 +1,46 @@
 ActiveAdmin.register Appointment do
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
+  permit_params :date, :time, :patient_id, :notes, :status
 
+  index do
+    selectable_column
+    column :date
+    column :time do |appointment|
+      appointment.friendly_time
+    end
+    column :patient do |appointment|
+      p = appointment.patient
+      link_to "NAME: #{p.last_name}, #{p.first_name} #{p.middle_name}<br>DOB: #{p.date_of_birth}".html_safe, admin_patient_path(p)
+    end
+    column :notes
+    column :status
+    actions
+  end
+
+  form do |f|
+    f.semantic_errors *f.object.errors.keys
+    f.inputs 'Appointment Details' do
+      patient_collection = Patient.order(:last_name).map{|p| ["#{p.id} - #{p.last_name}, #{p.first_name} #{p.middle_name} (DOB: #{p.date_of_birth})", p.id ]}
+      f.input :patient_id, label: 'Patient', :as => :select, :collection => patient_collection
+      f.input :date, :as => :datepicker
+      f.input :time, :as => :time_picker
+      f.input :notes
+    end
+    f.actions
+  end
+
+  show do
+    panel 'Appointment Details' do
+      attributes_table_for appointment do
+        row :patient do
+          p = appointment.patient
+          link_to "#{p.id} - #{p.last_name}, #{p.first_name} (DOB: #{p.date_of_birth})", admin_patient_path(p)
+        end
+        row :date
+        row :time do
+          appointment.friendly_time
+        end
+        row :notes
+      end
+    end
+  end
 end
