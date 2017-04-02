22e3e60b9dfc590ea3d77d07049fcee6c9b6e077
Shay Narang <shaynarang@admins-MacBook-Pro-3.local>
2017-04-01 23:25:26 -0500

    validate presence, update convenience method

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -2,7 +2,9 @@ class Patient < ApplicationRecord
   has_many :medical_records
   has_many :appointments
 
+  validates_presence_of :first_name, :last_name, :date_of_birth
+
   def name
-    "#{last_name}, #{first_name} #{middle_name}"
+    "#{last_name}, #{first_name} #{middle_name}".truncate(50).strip
   end
 end
