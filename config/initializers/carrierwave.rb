9d3045b0a79dd2a1b93c4b6a757ec54cfa0c662b
Shay Narang <shaynarang@admins-MacBook-Pro-3.local>
2017-04-02 18:10:53 -0500

    add comma

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,7 +12,7 @@ if Rails.env.production?
       # configuration for Amazon S3
       :provider              => 'AWS',
       :aws_access_key_id     => ENV['S3_KEY'],
-      :aws_secret_access_key => ENV['S3_SECRET']
+      :aws_secret_access_key => ENV['S3_SECRET'],
       :region                => 'us-west-2'
     }
 
