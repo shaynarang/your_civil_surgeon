69d79811e4762bd32d1c132f97098830d1afa5fa
Shay Narang <shaynarang@admins-MacBook-Pro-3.local>
2017-04-25 20:49:58 -0500

    update filename

--- app/controllers/medical_records_controller.rb
+++ app/controllers/medical_records_controller.rb
@@ -3,15 +3,11 @@ class MedicalRecordsController < ApplicationController
     medical_record = MedicalRecord.find(params[:id])
     scan = medical_record.scan
     return unless scan && scan.try(:file).exists?
-    patient_id = medical_record.patient_id
-    date_of_service = medical_record.date_of_service
-    kind = medical_record.kind.downcase
-    filename = "#{patient_id}_#{date_of_service}_#{kind}"
-    send_file(scan.url,
-          :filename => filename,
+    send_file(scan.file.path,
+          :filename => scan.path.split('/').last,
           :type => scan.file.content_type,
           :disposition => 'attachment',
-          :url_based_filename => true)
+          :url_based_filename => false)
   end
 end
 
