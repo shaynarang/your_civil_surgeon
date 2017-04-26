ad9e1d0da64e8409a941a55e282b19d8a826c7f7
Shay Narang <shaynarang@admins-MacBook-Pro-3.local>
2017-04-25 22:31:17 -0500

    add method to calculate age from date of birth

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -7,4 +7,11 @@ class Patient < ApplicationRecord
   def name
     "#{last_name}, #{first_name} #{middle_name}".truncate(50).strip
   end
+
+  def age
+    # http://stackoverflow.com/questions/819263/get-persons-age-in-ruby/5951744
+    now = Time.now.utc.to_date
+    now.year - date_of_birth.year - ((now.month > date_of_birth.month || (now.month == date_of_birth.month && now.day >= date_of_birth.day)) ? 0 : 1)
+  end
+
 end
