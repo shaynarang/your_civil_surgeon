9efc76da09bf6a59be665aa103c8d3be747414b0
Shay Narang <shaynarang@admins-MacBook-Pro-3.local>
2017-04-25 22:47:51 -0500

    remove line

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -13,5 +13,4 @@ class Patient < ApplicationRecord
     now = Time.now.utc.to_date
     now.year - date_of_birth.year - ((now.month > date_of_birth.month || (now.month == date_of_birth.month && now.day >= date_of_birth.day)) ? 0 : 1)
   end
-
 end
