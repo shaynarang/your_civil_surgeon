abec84de52e9e23f790fd2cc00b4c647e755efd0
Shay Narang <shaynarang@admins-MacBook-Pro-3.local>
2017-05-30 20:08:45 -0500

    add formatted time helper methods

--- app/models/unavailable_block.rb
+++ app/models/unavailable_block.rb
@@ -1,2 +1,13 @@
 class UnavailableBlock < ApplicationRecord
+  def formatted_time time
+    read_attribute(time) ? read_attribute(time).strftime('%I:%M%p') : nil
+  end
+
+  def start_time
+    formatted_time(:start_time)
+  end
+
+  def end_time
+    formatted_time(:end_time)
+  end
 end
