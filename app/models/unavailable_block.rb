8fc9402ad5b7c9505c746a077811130bda667efd
Shay Narang <shaynarang@admins-MacBook-Pro-3.local>
2017-05-31 15:46:14 -0500

    add validations, scope, and ranges

--- app/models/unavailable_block.rb
+++ app/models/unavailable_block.rb
@@ -1,4 +1,11 @@
 class UnavailableBlock < ApplicationRecord
+
+  validates_presence_of :start_date, :end_date
+
+  validate :end_date_after_start_date?
+
+  scope :contains_date, -> (date) { where('start_date <= ? AND end_date >= ?', date, date) }
+
   def formatted_time time
     read_attribute(time) ? read_attribute(time).strftime('%I:%M%p') : nil
   end
@@ -9,5 +16,25 @@ class UnavailableBlock < ApplicationRecord
 
   def end_time
     formatted_time(:end_time)
+  end
+
+  def start_date_time
+    (start_date.to_s + " " + start_time.to_s).to_datetime
+  end
+
+  def end_date_time
+    (end_date.to_s + " " + end_time.to_s).to_datetime
+  end
+
+  def time_range
+    [start_time, end_time]
+  end
+
+  private
+
+  def end_date_after_start_date?
+    if end_date < start_date
+      errors.add :end_date, 'must be after start date'
+    end
   end
 end
