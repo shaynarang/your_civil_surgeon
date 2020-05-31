4d0252d9fce7ed1c2732ff4e44fb56a05f26d79e
Shay Narang <shaynarang@admins-MBP-3.hsd1.tn.comcast.net>
2020-05-30 23:40:18 -0500

    modify files per rails upgrade

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,9 @@ Bundler.require(*Rails.groups)
 
 module YourCivilSurgeon
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
+
     config.generators do |g|
       g.test_framework  false
     end
